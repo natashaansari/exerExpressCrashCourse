50165ceb179ae970c83c9f4d2d01d7143b94f3df
natashaansari <natasha.ansari@utah.edu>
2020-12-06 13:15:36 -0700

    done!

---
+++ express_crash_course/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Jane Smith',
+        email: 'jane@gmail.com',
+        status: 'inactive'
+    },
+    {
+        id: 3,
+        name: 'Shannon Smith',
+        email: 'shannon@gmail.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
